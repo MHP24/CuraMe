6086023dfae5d97b894ecf71caf67b6289ce35be
MHP24 <85582472+MHP24@users.noreply.github.com>
2022-12-08 13:48:07 -0300

    Queries interfaces added

--- src/utils/utils.module..ts
+++ src/utils/utils.module..ts
@@ -1,10 +1,12 @@
-export const insertRoles: any = {
+import { QueryHI } from '../interfaces/query-handler.interface';
+
+export const insertRoles: QueryHI = {
     3: 'INSERT INTO medico (id_especialidad, id_sucursal, rut) VALUES(?, ?, ?);',
     4: 'INSERT INTO secretaria (id_sucursal, rut) VALUES(?, ?);',
     5: 'INSERT INTO cajero (id_sucursal, rut) VALUES(?, ?);'
 }
 
-export const selectRoles: any = {
+export const selectRoles: QueryHI = {
     1: 'SELECT * FROM paciente WHERE rut = ?;',
     2: 'SELECT * FROM paciente WHERE rut = ?;',
     3: 'SELECT e.nombre_especialidad, s.direccion FROM medico r JOIN sucursal s ON s.id_sucursal = r.id_sucursal JOIN especialidad e ON r.id_especialidad = e.id_especialidad WHERE r.rut = ? LIMIT 1;',
@@ -12,19 +14,19 @@ export const selectRoles: any = {
     5: 'SELECT s.direccion FROM cajero r JOIN sucursal s on r.id_sucursal = s.id_sucursal WHERE r.rut = ?;'
 }
 
-export const deleteRoles: any = {
+export const deleteRoles: QueryHI = {
     3: 'DELETE FROM medico WHERE rut = ?',
     4: 'DELETE FROM secretaria WHERE rut = ?',
     5: 'DELETE FROM cajero WHERE rut = ?'
 }
 
-export const updateRoles: any = {
+export const updateRoles: QueryHI = {
     3: 'UPDATE medico SET id_especialidad = ?, id_sucursal = ? WHERE rut = ?',
     4: 'UPDATE secretaria SET id_sucursal = ? WHERE rut = ?',
     5: 'UPDATE cajero SET id_sucursal = ? WHERE rut = ?'
 }
 
-export const role: any = {
+export const role: QueryHI = {
     1: 'Paciente',
     2: 'Administrador',
     3: 'Médico',
